83a4e54187a6a441423153549f90001babec09a6
Abhay Raj <abhayraj702unique@gmail.com>
2026-01-08 13:50:05 +0530

    .

---
+++ Functions/fn3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+int x = 8, z =8;
+cout<< sum(x, z);
+}
---
+++ Functions/minArray.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int fun(){
+    return 2;
+}
+int main(){
+cout<<fun();
+}
