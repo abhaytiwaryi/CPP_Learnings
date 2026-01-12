72cacdf79a26e4785c9598441ec2d7d0a23c9ad9
Abhay Raj <abhayraj702unique@gmail.com>
2026-01-12 05:59:28 +0530

    .

--- Functions/sumOfDigits.cpp
+++ Functions/sumOfDigits.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
+    int num, sum = 0;
+    cout<<"Enter the number: ";
+    cin >> num;
 
+    while (num>0){
+        int digit = num%10;
+        sum +=digit;
+        num = num/10;
+    }
+    cout << "Sum of digits = " << sum << endl;
+    return 0;
 }
